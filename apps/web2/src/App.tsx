f5a76ab61398f07a658e7b29e106ddc69ba2a296
Alexander Johansson <alexander@n1s.se>
2025-01-17 04:13:13 +0100

    cool

--- apps/web2/src/App.tsx
+++ apps/web2/src/App.tsx
@@ -2,7 +2,7 @@ import { trpc } from './lib/trpc'
 
 export function App() {
   const { data } =
-    trpc.router45.quantum.multiverse.infiniteRecursion.recurse.useQuery({
+    trpc.router0.quantum.multiverse.infiniteRecursion.recurse.useQuery({
       depth: 1,
       data: { hello: 'world' },
       callback: () => Promise.resolve(),
