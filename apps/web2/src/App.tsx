e925d64bfd42ed427eca5726b4f9d3ff966850fe
Alexander Johansson <alexander@n1s.se>
2025-01-17 03:02:39 +0100

    wip
    bun run build  12.24s user 0.57s system 141% cpu 9.025 total

--- apps/web2/src/App.tsx
+++ apps/web2/src/App.tsx
@@ -2,11 +2,12 @@ import { trpc } from './lib/trpc'
 
 export function App() {
   const { data } =
-    trpc.router0.quantum.multiverse.infiniteRecursion.recurse.useQuery({
+    trpc.router45.quantum.multiverse.infiniteRecursion.recurse.useQuery({
       depth: 1,
       data: { hello: 'world' },
       callback: () => Promise.resolve(),
     })
 
   return <h1>{data ?? 'Loading...'}</h1>
+  //           ^?
 }
