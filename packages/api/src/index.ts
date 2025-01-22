81ea323bafca3ffc9bef96960db420f0484b1587
Alexander Johansson <alexander@n1s.se>
2025-01-22 10:00:38 +0100

    wip

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -3,105 +3,11 @@ import { router } from '@org/trpc';
 import { router001 } from '@org/router001';
 import { router002 } from '@org/router002';
 import { router003 } from '@org/router003';
-import { router004 } from '@org/router004';
-import { router005 } from '@org/router005';
-import { router006 } from '@org/router006';
-import { router007 } from '@org/router007';
-import { router008 } from '@org/router008';
-import { router009 } from '@org/router009';
-import { router010 } from '@org/router010';
-import { router011 } from '@org/router011';
-import { router012 } from '@org/router012';
-import { router013 } from '@org/router013';
-import { router014 } from '@org/router014';
-import { router015 } from '@org/router015';
-import { router016 } from '@org/router016';
-import { router017 } from '@org/router017';
-import { router018 } from '@org/router018';
-import { router019 } from '@org/router019';
-import { router020 } from '@org/router020';
-import { router021 } from '@org/router021';
-import { router022 } from '@org/router022';
-import { router023 } from '@org/router023';
-import { router024 } from '@org/router024';
-import { router025 } from '@org/router025';
-import { router026 } from '@org/router026';
-import { router027 } from '@org/router027';
-import { router028 } from '@org/router028';
-import { router029 } from '@org/router029';
-import { router030 } from '@org/router030';
-import { router031 } from '@org/router031';
-import { router032 } from '@org/router032';
-import { router033 } from '@org/router033';
-import { router034 } from '@org/router034';
-import { router035 } from '@org/router035';
-import { router036 } from '@org/router036';
-import { router037 } from '@org/router037';
-import { router038 } from '@org/router038';
-import { router039 } from '@org/router039';
-import { router040 } from '@org/router040';
-import { router041 } from '@org/router041';
-import { router042 } from '@org/router042';
-import { router043 } from '@org/router043';
-import { router044 } from '@org/router044';
-import { router045 } from '@org/router045';
-import { router046 } from '@org/router046';
-import { router047 } from '@org/router047';
-import { router048 } from '@org/router048';
-import { router049 } from '@org/router049';
-import { router050 } from '@org/router050';
 
 export const appRouter = router({
   router001,
   router002,
-  router003,
-  router004,
-  router005,
-  router006,
-  router007,
-  router008,
-  router009,
-  router010,
-  router011,
-  router012,
-  router013,
-  router014,
-  router015,
-  router016,
-  router017,
-  router018,
-  router019,
-  router020,
-  router021,
-  router022,
-  router023,
-  router024,
-  router025,
-  router026,
-  router027,
-  router028,
-  router029,
-  router030,
-  router031,
-  router032,
-  router033,
-  router034,
-  router035,
-  router036,
-  router037,
-  router038,
-  router039,
-  router040,
-  router041,
-  router042,
-  router043,
-  router044,
-  router045,
-  router046,
-  router047,
-  router048,
-  router049,
-  router050
+  router003
 });
 
 export type AppRouter = typeof appRouter;
