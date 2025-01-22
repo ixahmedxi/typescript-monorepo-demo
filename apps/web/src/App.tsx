c7a4b409ecb401f9b6f04dac90e79c665b8c9f08
Alexander Johansson <alexander@n1s.se>
2025-01-22 12:21:41 +0100

    wip

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -6,7 +6,5 @@ export function App() {
   if (q1.data === 'bar') {
   }
 
-  // 1. run `bun dev`
-  // 2. change `generated-routers/router0/src/index.ts` and see it update here
   return <h1>{q1.data ?? 'Loading...'}</h1>
 }
